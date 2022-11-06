2a97d8a39ff90ecdd27a221ef983c71f8bcc81ca
Thanh Nguyen <nct1120@gmail.com>
2022-11-06 14:28:06 +0700

    SEABORN

--- test_auto_detect.py
+++ test_auto_detect.py
@@ -1,11 +1,12 @@
-from sqlalchemy import true
+import time
 from __init__ import *
 
 def main():
     ark = Arknights()
     farming = False
+    start = None
     while(1):
-        type, reg = ark.detect_screen()
+        type, reg = ark.detect_screen("seaborn")
         if(type != None):
             if(not farming):
                 print(type, reg)
